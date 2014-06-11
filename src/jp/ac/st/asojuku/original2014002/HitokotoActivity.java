99f7dd4c7d8c4d475eaaddf7a01e82b702e034ff
student <student@PC111S03.fgakusei.local>
2014-06-11 15:20:13 +0900

    追加

--- src/jp/ac/st/asojuku/original2014002/HitokotoActivity.java
+++ src/jp/ac/st/asojuku/original2014002/HitokotoActivity.java
@@ -1,7 +1,9 @@
 package jp.ac.st.asojuku.original2014002;
 
 import jp.ac.st.asojukuoriginal2014002.R;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HitokotoActivity extends MainActivity {
 
@@ -10,6 +12,17 @@ public class HitokotoActivity extends MainActivity {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.hitokotoactivity);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		Intent intent = this.getIntent();
+		String strHitokoto = intent.getStringExtra("hitokoto");
+		TextView txvHITOKOTO = (TextView)findViewById(R.id.txt1);
+		txvHITOKOTO.setText(strHitokoto);
 	}
 
 
--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -1,9 +1,10 @@
 package jp.ac.st.asojuku.original2014002;
 
-
 import jp.ac.st.asojukuoriginal2014002.R;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -12,6 +13,8 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
 
 	@Override
 	public void onClick(View v) {
@@ -23,8 +26,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		switch(v.getId()){
 
 		case R.id.btn_add:
-			intent = new Intent(MainActivity.this, AddActivity.class);
-			startActivity(intent);
+
+			if(inputMsg!=null && !inputMsg.isEmpty()){
+				helper.insertHitokoto(sdb, inputMsg);
+			}
+			etv.setText("");
 			break;
 
 		case R.id.button1:
@@ -33,7 +39,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 			break;
 
 		case R.id.btn_ck:
+			String strHitokoto = helper.selectRandomHitokoto(sdb);
+
 			intent = new Intent(MainActivity.this, HitokotoActivity.class);
+			intent.putExtra("hitokoto", strHitokoto);
+
 			startActivity(intent);
 			break;
 		}
@@ -48,14 +58,49 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
 
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Button btn = (Button)findViewById(R.id.btn_add);
+		Button btn1 = (Button)findViewById(R.id.btn_ck);
+		Button btn2 = (Button)findViewById(R.id.button1);
 		btn.setOnClickListener(this);
+		btn1.setOnClickListener(this);
+		btn2.setOnClickListener(this);
 	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		Button btnENTRY = (Button)findViewById(R.id.btn_add);
+		btnENTRY.setOnClickListener(this);
+
+		Button btnMAINTE = (Button)findViewById(R.id.button1);
+		btnENTRY.setOnClickListener(this);
+
+		Button btnCHECK = (Button)findViewById(R.id.btn_ck);
+		btnENTRY.setOnClickListener(this);
+
+		if(sdb == null) {
+				helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			return;
+
+		}
+	}
+
+
+
+
+
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
